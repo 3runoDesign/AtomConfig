ff7dd1d9c1ea97d7eed49d596e50ac151ac80959
Bruno Fernando <bruno2fernando@gmail.com>
2017-01-02 16:43:34 -0300

    Add packages.sh

---
+++ resource/packages.sh
@@ -0,0 +1,1 @@
+apm install atom-alignment atom-autocomplete-php atom-wrap-in-tag cobalt2-syntax docblockr editorconfig emmet file-icons hyperclick language-blade language-pug pigments sort-lines todo-show
